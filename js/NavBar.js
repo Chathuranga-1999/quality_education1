c9c4892ae007f8b9b95175cf4c3e50e993d80ffe
chathuranga_senarathne <chathu.redfox@gmail.com>
2022-02-16 23:11:38 +0530

    add js file

---
+++ js/NavBar.js
@@ -0,0 +1,27 @@
+
+function Home() 
+{
+	Window.location.href="#"
+}
+
+function Service()
+{
+	var myWindow =window.open("","","")
+	myWindow.location.href="#"
+}
+
+function Register()
+{
+	Window.location.href="#"
+}
+
+function Login()
+{
+	Window.location.href="#"
+}
+
+function AboutUs()
+{
+	var myWindow =window.open("","","")
+	myWindow.location.href="#"
+}
