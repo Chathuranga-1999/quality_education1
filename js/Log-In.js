175bcc81c7a18264d7f2696062cf3e99dd86be3d
Avishka Gihan <avishkag18@gmail.com>
2022-02-18 12:01:15 +0530

    updating nav

--- js/Log-In.js
+++ js/Log-In.js
@@ -1,27 +1,38 @@
+var ul_custom = document.querySelector(".ul_custom");
 
-function Home() 
-{
-	Window.location.href="#"
+ul_custom.addEventListener("click", btn_click, false);
+
+function btn_click(e) {
+	if(e.target !== e.currentTargert){
+		var clickedItem = e.target.id;
+		var element = document.getElementById(clickedItem);
+  		element.classList.add("press");
+
+  		switch(element) {
+  			case Home:
+    			window.location='Home.html';
+    		break;
+  			
+  			case Service:
+    			window.location='Service.html';
+    		break;
+
+    		case Register:
+    			window.location='Register.html';
+    		break;
+
+    		case Log_In:
+    			window.location='Log_In.html';
+    		break;
+
+    		case About_us:
+    			window.location='About_Us.html';
+    		break;
+  			
+		}
+	}
+
+	e.stopPropagation();
 }
 
-function Service()
-{
-	var myWindow =window.open("","","")
-	myWindow.location.href="#"
-}
 
-function Register()
-{
-	Window.location.href="#"
-}
-
-function Login()
-{
-	Window.location.href="#"
-}
-
-function AboutUs()
-{
-	var myWindow =window.open("","","")
-	myWindow.location.href="#"
-}
